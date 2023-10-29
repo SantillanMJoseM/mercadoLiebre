99f5badf70768e22704397e8e51750f936b3c2dc
Santillan Medina Jose Maria <josemedina111@gmail.com>
2023-10-29 12:12:40 -0300

    Version inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(3030, ()=>{
+    console.log('Servidor ejecutandose correctamente');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname ,'/views/index.html'));
+});
